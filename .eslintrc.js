2f2d6030c14f4be1874b03047558cd968c43093c
Skyler Dong <dongskyler@gmail.com>
2020-09-19 17:27:45 -0500

    Update Eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    'shared-node-browser': true,
     es2020: true,
     mocha: true,
   },
@@ -14,9 +15,16 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'func-names': 0,
     'prettier/prettier': 2,
   },
+  overrides: [
+    {
+      files: ['*.spec.js', '*.test.js'],
+      rules: {
+        'func-names': 0,
+      },
+    },
+  ],
   plugins: ['mocha', 'chai-friendly', 'prettier'],
   extends: [
     'airbnb-base',
